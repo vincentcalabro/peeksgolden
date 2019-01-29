e99023f0575be11cd44752693bafa95f77f16881
Vincent Calabro <vinny@1stdibs.com>
2019-01-28 23:03:55 -0500

    Revert "freelancer theme"
    This reverts commit 454c4aa854f8ec070ff532e34efd45d567b64c7d.

--- peeksgolden-app/src/App.vue
+++ peeksgolden-app/src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <v-app>
-    <core-toolbar />
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
 
-    <core-view />
-
-    <core-footer />
+    <v-content>
+      <HelloWorld/>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld'
+
 export default {
+  name: 'App',
   components: {
-    CoreFooter: () => import('@/components/core/Footer'),
-    CoreToolbar: () => import('@/components/core/Toolbar'),
-    CoreView: () => import('@/components/core/View')
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
--- peeksgolden-app/src/plugins/vuetify.js
+++ peeksgolden-app/src/plugins/vuetify.js
@@ -1,13 +1,7 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
-  theme: {
-    primary: '#D3C3BC',
-    secondary: '#BFADA5',
-    accent: '#D8EBF1',
-    info: '#5B5C79'
-  },
-  iconfont: 'mdi'
+  iconfont: 'md',
 })
